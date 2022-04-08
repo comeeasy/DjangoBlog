f208293a3f581f48230ebb4b1f38fe7f6913ce4a
comeeasy <zz1236zz@naver.com>
2022-04-08 16:14:52 +0900

    Add: category model

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Category
 
 # Register your models here.
 admin.site.register(Post)
+
+class CategoryAdmin(admin.ModelAdmin):
+    # name field 값으로 slug 자동으로 생성
+    prepopulated_fields = {"slug": ("name",)}
+
+admin.site.register(Category, CategoryAdmin)
--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,18 @@ import os.path
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     contents = models.TextField()
@@ -13,7 +24,8 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f"[{self.pk}] {self.title} :: {self.author}"
